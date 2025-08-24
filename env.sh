f1ada328c1b289a8fe14875de8f941fabe68da8a
raghavagrawal52 <raghavagrawal52@users.noreply.github.com>
2025-08-24 18:27:57 +0530

    updating env.sh

--- env.sh
+++ env.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 # Initial setup (must)
-sudo pacman -S neovim ghostty hyprland tmux wine --needed --noconfirm
+sudo pacman -S git openssh neovim ghostty hyprland tmux --needed --noconfirm
 
 # terminal packages
-sudo pacman -S tree fzf ripgrep bluetui man-db --needed --noconfirm
+sudo pacman -S wget tree fzf ripgrep bluetui man-db --needed --noconfirm
 
 #utility
 sudo pacman -S zathura zathura-pdf-mudf --needed --noconfirm
-
-# dev
-sudo pacman -S opam --needed --noconfirm
-opam init -y
-opam install utop dune ocaml-lsp-server ocamlformat ocp-indent
-
-#AUR packages
